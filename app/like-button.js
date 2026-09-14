e5aedc1b1a3b2bf9a10b9b00a874dd72528737d0
lud1903 <ludmyla.vitoria@escolar.ifrn.edu.br>
2026-09-14 17:25:23 +0000

    capitulo 10- move botão Like para componente

--- app/like-button.js
+++ app/like-button.js
@@ -1,1 +1,7 @@
-export default function LikeButton() {}
+export default function LikeButton() {
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return <button onClick={handleClick}>Like ({likes})</button>;
+}
