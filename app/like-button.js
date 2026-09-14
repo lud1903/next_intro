d9796295395967f603ab739e2a663377025d5219
lud1903 <ludmyla.vitoria@escolar.ifrn.edu.br>
2026-09-14 17:26:09 +0000

    capitulo 10- adiciona estado ao componente LikeButton

--- app/like-button.js
+++ app/like-button.js
@@ -1,4 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+
   function handleClick() {
     setLikes(likes + 1);
   }
